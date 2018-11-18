9506043107f4725642f69812442d38b17a7fe8a5
chenjinheng <1640297066@qq.com>
2018-11-18 19:51:38 +0800

    第二次提交

--- app/src/main/java/com/example/vediotest/activity/MainActivity.java
+++ app/src/main/java/com/example/vediotest/activity/MainActivity.java
@@ -4,16 +4,17 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 import com.example.vediotest.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 startMainActivity();
@@ -25,5 +26,17 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,MainActivity1.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        startMainActivity();
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
     }
 }
