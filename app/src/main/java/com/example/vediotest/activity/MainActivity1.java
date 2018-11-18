372d7102b889917ee081b70688254db47f2f7c96
chenjinheng <1640297066@qq.com>
2018-11-18 20:45:50 +0800

    主界面

--- app/src/main/java/com/example/vediotest/activity/MainActivity1.java
+++ app/src/main/java/com/example/vediotest/activity/MainActivity1.java
@@ -2,15 +2,20 @@ package com.example.vediotest.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
 
 import com.example.vediotest.R;
 
 public class MainActivity1 extends AppCompatActivity {
-
+    private FrameLayout frameLayout;
+    private RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
-
+        frameLayout = (FrameLayout) findViewById(R.id.fl_main_layout);
+        radioGroup = (RadioGroup) findViewById(R.id.rg_bottom_tag);
+        radioGroup.check(R.id.rb_video);
     }
 }
